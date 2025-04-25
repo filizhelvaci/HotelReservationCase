6558874d7a030b8237c7f8d2304a0bc54515f881
Filizkh <filizhelvaci82@gmail.com>
2025-04-25 17:27:44 +0300

    HotelServiceImpl created

---
+++ HotelService/src/main/java/com/example/hotelservice/service/impl/HotelServiceImpl.java
@@ -0,0 +1,12 @@
+package com.example.hotelservice.service.impl;
+
+import com.example.hotelservice.service.HotelService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+class HotelServiceImpl implements HotelService {
+
+
+}
