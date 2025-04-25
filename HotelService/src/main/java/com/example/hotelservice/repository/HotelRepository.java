7343bf17399c57df239409cb611cbc04f6bbdae5
Filizkh <filizhelvaci82@gmail.com>
2025-04-25 15:27:07 +0300

    Hotel repository created

---
+++ HotelService/src/main/java/com/example/hotelservice/repository/HotelRepository.java
@@ -0,0 +1,9 @@
+package com.example.hotelservice.repository;
+
+import com.example.hotelservice.model.entitiy.Hotel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HotelRepository extends JpaRepository<Hotel, Long> {
+}
