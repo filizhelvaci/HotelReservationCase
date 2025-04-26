6f756fdee3a59a7b3c881076c614d6948aff1628
Filizkh <filizhelvaci82@gmail.com>
2025-04-26 09:42:18 +0300

    Hotel controller created

--- HotelService/src/main/java/com/example/hotelservice/controller/HotelController.java
+++ HotelService/src/main/java/com/example/hotelservice/controller/HotelController.java
@@ -9,18 +9,26 @@ import com.example.hotelservice.model.response.HotelsSummaryResponse;
 import com.example.hotelservice.service.HotelService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Validated
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1")
 public class HotelController {
 
-    private HotelService hotelService;
+    private final HotelService hotelService;
 
     @GetMapping("/hotels")
     public HotelRsResponse<List<HotelsResponse>> findAll() {
