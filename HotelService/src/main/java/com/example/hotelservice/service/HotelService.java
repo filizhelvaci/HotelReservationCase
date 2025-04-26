963dc9475c499d6eb131ecf1d8af269c6e4d4d7c
Filizkh <filizhelvaci82@gmail.com>
2025-04-26 08:05:10 +0300

    HotelService created

--- HotelService/src/main/java/com/example/hotelservice/service/HotelService.java
+++ HotelService/src/main/java/com/example/hotelservice/service/HotelService.java
@@ -1,4 +1,24 @@
 package com.example.hotelservice.service;
 
+import com.example.hotelservice.model.request.HotelCreateRequest;
+import com.example.hotelservice.model.request.HotelUpdateRequest;
+import com.example.hotelservice.model.response.HotelResponse;
+import com.example.hotelservice.model.response.HotelsResponse;
+import com.example.hotelservice.model.response.HotelsSummaryResponse;
+
+import java.util.List;
+
 public interface HotelService {
+
+    List<HotelsResponse> findAll();
+
+    List<HotelsSummaryResponse> findSummaryAll();
+
+    HotelResponse findById(Long id);
+
+    void create(HotelCreateRequest hotelCreateRequest);
+
+    void update(Long id, HotelUpdateRequest hotelUpdateRequest);
+
+    void delete(Long id);
 }
