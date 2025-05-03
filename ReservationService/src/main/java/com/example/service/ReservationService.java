7e4b4e3ddd1b7da9015165be5a67ec89f3bcbb4e
Filizkh <filizhelvaci82@gmail.com>
2025-05-03 16:30:21 +0300

    findAll() method added to ReservationService

--- ReservationService/src/main/java/com/example/service/ReservationService.java
+++ ReservationService/src/main/java/com/example/service/ReservationService.java
@@ -1,12 +1,17 @@
 package com.example.service;
 
+import com.example.model.entity.Reservation;
 import com.example.model.request.ReservationCreateRequest;
 import com.example.model.request.ReservationUpdateRequest;
 import com.example.model.response.ReservationResponse;
 
+import java.util.List;
+
 public interface ReservationService {
 
     ReservationResponse findById(Long id);
+
+    List<Reservation> findAll();
 
     void create(ReservationCreateRequest reservationCreateRequest);
 
